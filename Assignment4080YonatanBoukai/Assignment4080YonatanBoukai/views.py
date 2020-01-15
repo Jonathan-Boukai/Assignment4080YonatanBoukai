c13225ad2a5bc4f40fda78890a16f092662ad1e8
JB11🌮 Boukai <jonathanboukai1@gmail.com>
2020-01-15 16:13:47 +0200

    JB 15.1.20

---
+++ Assignment4080YonatanBoukai/Assignment4080YonatanBoukai/views.py
@@ -0,0 +1,46 @@
+"""
+Routes and views for the flask application.
+"""
+
+from datetime import datetime
+from flask import render_template
+from Assignment4080YonatanBoukai import app
+
+@app.route('/')
+@app.route('/home')
+def home():
+    """Renders the home page."""
+    return render_template(
+        'index.html',
+        title='Home Page',
+        year=datetime.now().year,
+    )
+
+@app.route('/contact')
+def contact():
+    """Renders the contact page."""
+    return render_template(
+        'contact.html',
+        title='Contact',
+        year=datetime.now().year,
+        message='Your contact page.'
+    )
+@app.route('/about')
+def about():
+    """Renders the about page."""
+    return render_template(
+        'about.html',
+        title='About',
+        year=datetime.now().year,
+        message='Your about page.'
+    )
+
+@app.route('/gallery')
+def gallery():
+    """Renders the about page."""
+    return render_template(
+        'gallery.html',
+        title='Gallery',
+        year=datetime.now().year,
+        message='The top FIFA 19 rated players.'
+    )
